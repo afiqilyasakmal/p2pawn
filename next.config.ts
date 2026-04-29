2d4ad87f550775744b5cab5f80dcc6ce73d7ca78
Afiq Ilyasa Akmal <afiq.ilyasa@ui.ac.id>
2026-04-29 07:04:57 +0700

    fix csp

--- next.config.ts
+++ next.config.ts
@@ -4,26 +4,7 @@ const nextConfig: NextConfig = {
   ...(process.env.NODE_ENV === "development" && {
     allowedDevOrigins: ["192.168.100.240"],
   }),
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value:
-              "default-src 'self'; " +
-              "script-src 'self'; " +
-              "style-src 'self' 'unsafe-inline'; " +
-              "connect-src 'self' wss://0.peerjs.com https://0.peerjs.com; " +
-              "img-src 'self' data:; " +
-              "font-src 'self' data:; " +
-              "frame-ancestors 'none';",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
+
